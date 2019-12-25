42063c34ddc77bbf5572a2bd63dbcadf11542a59
Abdul Aliyev <aliyev_abdul@bah.com>
2019-12-25 14:48:46 -0500

    Set up Sendgrid email service

--- utils/Email.js
+++ utils/Email.js
@@ -20,7 +20,13 @@ module.exports = class Email {
     /*==========*/
     if (this.service === 'sendgrid') {
       // Sendgrid
-      return 1;
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD
+        }
+      });
     } 
     
     /*=======*/
@@ -82,7 +88,7 @@ module.exports = class Email {
       html,
       text: htmlToText.fromString(html)
     };
-
+    
     // 3) Create a transport and send email
     await this.newTransport().sendMail(mailOptions);
   }
@@ -95,45 +101,3 @@ module.exports = class Email {
     await this.send('passwordReset', 'Your password reset token (valid for only 10 minutes!)');
   }
 };
-
-// const sendEmail = async options => {
-//   // 1) Create a transporter
-//   let transporter;
-//   if (process.env.EMAIL_SERVICE === 'gmail') {
-//     // !!! MAKE SURE TO Activate Gmail "less secure appS" option!
-//     transporter = nodemailer.createTransport({
-//       service: 'Gmail',
-//       auth: {
-//         user: process.env.GMAIL_USERNAME,
-//         pass: process.env.GMAIL_PASSWORD
-//       }
-//     });
-//   } else if (process.env.EMAIL_SERVICE === 'mailtrap') {
-//     transporter = nodemailer.createTransport({
-//       host: process.env.MAILTRAP_HOST,
-//       port: process.env.MAILTRAP_PORT,
-//       auth: {
-//         user: process.env.MAILTRAP_USERNAME,
-//         pass: process.env.MAILTRAP_PASSWORD
-//       }
-//     });
-//   } else {
-//     // If there is no EMAIL_SERVICE env variable, then throw an error
-//     throw new Error(
-//       'Email service has not been specified in environmental variables.'
-//     );
-//   }
-//   // 2) Define the email options
-//   const mailOptions = {
-//     from: options.from || 'App App <app@app.com>',
-//     to: options.to,
-//     subject: options.subject,
-//     text: options.message
-//     // html: TODO -> Specify html later...
-//   };
-
-//   // 3) Actually send the email
-//   await transporter.sendMail(mailOptions);
-// };
-
-// module.exports = sendEmail;
