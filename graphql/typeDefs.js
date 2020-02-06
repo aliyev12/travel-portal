e797f41d8f3e32e8333c1036e084100c57d2a57f
Abdul Aliyev <aaa7c4@gmail.com>
2020-02-05 19:06:17 -0500

    Add gql

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -6,10 +6,10 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type Tour {
-    name: String
+    name: String!
     duration: Int
     maxGroupSize: Int
-    difficulty: String
+    difficulty: String!
     ratingsQuantity: Int
     summary: String
     description: String
@@ -51,32 +51,6 @@ const typeDefs = gql`
 module.exports = typeDefs;
 
 // const tourShape = {
-//   name: {
-//     type: String,
-//     required: [true, "A tour must have a name"],
-//     unique: true,
-//     trim: true,
-//     maxlength: [40, "A tour name must have less or equal than 40 characters"],
-//     minlength: [10, "A tour name must have more or equal than 10 characters"]
-//     // , validate: [validator.isAlpha, 'Tour name must only contain characters'] // Example how to use validator library
-//   },
-//   duration: Number,
-//   maxGroupSize: Number,
-//   difficulty: {
-//     type: String,
-//     required: [true, "A tour must have a difficulty"],
-//     enum: getDifficultyEnum()
-//   },
-//   ratingsQuantity: {
-//     type: Number,
-//     default: 0
-//   },
-//   summary: String,
-//   description: {
-//     type: String,
-//     trim: true
-//   },
-//   images: [String],
 //   createdAt: {
 //     type: Date,
 //     default: Date.now()
@@ -146,74 +120,87 @@ module.exports = typeDefs;
 //   ]
 // };
 /*
-{
-
-    "createdAt": "2019-12-28T16:14:31.664Z",
-    "startDates": [
-        "2021-06-19T09:00:00.000Z",
-        "2021-07-20T09:00:00.000Z",
-        "2021-08-18T09:00:00.000Z"
-    ],
-    "secretTour": false,
-    "rating": 4.5,
-    "ratingsAverage": 4.8,
-    "guides": [],
-    "_id": "5c88fa8cf4afda39709c2955",
-    "name": "The Sea Explorer",
-    "duration": 7,
-    "maxGroupSize": 15,
-    "difficulty": "medium",
-    "price": 497,
-    "summary": "Exploring the jaw-dropping US east coast by foot and by boat",
-    "description": "Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nIrure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    "imageCover": "tour-2-cover.jpg",
-    "locations": [
-        {
-            "type": "Point",
-            "coordinates": [
-                -80.128473,
-                25.781842
-            ],
-            "_id": "5c88fa8cf4afda39709c2959",
-            "description": "Lummus Park Beach",
-            "day": 1
-        },
-        {
-            "type": "Point",
-            "coordinates": [
-                -80.647885,
-                24.909047
-            ],
-            "_id": "5c88fa8cf4afda39709c2958",
-            "description": "Islamorada",
-            "day": 2
-        },
-        {
-            "type": "Point",
-            "coordinates": [
-                -81.0784,
-                24.707496
-            ],
-            "_id": "5c88fa8cf4afda39709c2957",
-            "description": "Sombrero Beach",
-            "day": 3
-        },
-        {
-            "type": "Point",
-            "coordinates": [
-                -81.768719,
-                24.552242
-            ],
-            "_id": "5c88fa8cf4afda39709c2956",
-            "description": "West Key",
-            "day": 5
-        }
-    ],
-    "updatedAt": "2019-12-28T16:14:33.379Z",
-    "slug": "the-sea-explorer",
-    "updatedFromNow": "38 days and 9 hours and 54764 minutes ago",
-    "createdFromNow": "38 days and 9 hours and 54764 minutes ago",
-    "id": "5c88fa8cf4afda39709c2955"
-},
-
+            {
+                "startLocation": {
+                    "type": "Point",
+                    "description": "Miami, USA",
+                    "coordinates": [
+                        -80.185942,
+                        25.774772
+                    ],
+                    "address": "301 Biscayne Blvd, Miami, FL 33132, USA"
+                },
+                "ratingsQuantity": 4,
+                "images": [
+                    "tour-2-1.jpg",
+                    "tour-2-2.jpg",
+                    "tour-2-3.jpg"
+                ],
+                "createdAt": "2019-12-28T16:14:31.664Z",
+                "startDates": [
+                    "2021-06-19T09:00:00.000Z",
+                    "2021-07-20T09:00:00.000Z",
+                    "2021-08-18T09:00:00.000Z"
+                ],
+                "secretTour": false,
+                "rating": 4.5,
+                "ratingsAverage": 4.8,
+                "guides": [],
+                "_id": "5c88fa8cf4afda39709c2955",
+                "name": "The Sea Explorer",
+                "duration": 7,
+                "maxGroupSize": 15,
+                "difficulty": "medium",
+                "price": 497,
+                "summary": "Exploring the jaw-dropping US east coast by foot and by boat",
+                "description": "Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nIrure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                "imageCover": "tour-2-cover.jpg",
+                "locations": [
+                    {
+                        "type": "Point",
+                        "coordinates": [
+                            -80.128473,
+                            25.781842
+                        ],
+                        "_id": "5c88fa8cf4afda39709c2959",
+                        "description": "Lummus Park Beach",
+                        "day": 1
+                    },
+                    {
+                        "type": "Point",
+                        "coordinates": [
+                            -80.647885,
+                            24.909047
+                        ],
+                        "_id": "5c88fa8cf4afda39709c2958",
+                        "description": "Islamorada",
+                        "day": 2
+                    },
+                    {
+                        "type": "Point",
+                        "coordinates": [
+                            -81.0784,
+                            24.707496
+                        ],
+                        "_id": "5c88fa8cf4afda39709c2957",
+                        "description": "Sombrero Beach",
+                        "day": 3
+                    },
+                    {
+                        "type": "Point",
+                        "coordinates": [
+                            -81.768719,
+                            24.552242
+                        ],
+                        "_id": "5c88fa8cf4afda39709c2956",
+                        "description": "West Key",
+                        "day": 5
+                    }
+                ],
+                "updatedAt": "2019-12-28T16:14:33.379Z",
+                "slug": "the-sea-explorer",
+                "updatedFromNow": "38 days and 10 hours and 54765 minutes ago",
+                "createdFromNow": "38 days and 10 hours and 54765 minutes ago",
+                "id": "5c88fa8cf4afda39709c2955"
+            },
 */
--- models/tourModel.js
+++ models/tourModel.js
@@ -25,14 +25,14 @@ tourSchema.index({ startLocation: "2dsphere" });
 /* SET CREATED & UPDATED FROM NOW */
 /*================================*/
 
-// tourSchema.virtual('updatedFromNow').get(function() {
-//   if (!this.updatedAt) return;
-//   return fromNow(this.updatedAt);
-// });
-// tourSchema.virtual('createdFromNow').get(function() {
-//   if (!this.createdAt) return;
-//   return fromNow(this.createdAt);
-// });
+tourSchema.virtual("updatedFromNow").get(function() {
+  if (!this.updatedAt) return;
+  return fromNow(this.updatedAt);
+});
+tourSchema.virtual("createdFromNow").get(function() {
+  if (!this.createdAt) return;
+  return fromNow(this.createdAt);
+});
 
 /*============================================================*/
 /* CREATE REVIEWS PROP ON TOUR THAT ADDS REFERENCE TO REVIEWS */
@@ -42,45 +42,45 @@ tourSchema.index({ startLocation: "2dsphere" });
 // two way reference where each review refers to a tour and each tour
 // refers to review. Instead of manually setting this relationship, we can create
 // mongoose virtual field that does the same thing.
-// tourSchema.virtual('reviews', {
-//   ref: 'Review',
-//   foreignField: 'tour',
-//   localField: '_id'
-// });
+tourSchema.virtual("reviews", {
+  ref: "Review",
+  foreignField: "tour",
+  localField: "_id"
+});
 
 /*=====================*/
 /* SET SLUG - PRE SAVE */
 /*=====================*/
 
 // DOCUMENT MIDDLEWARE: runs before the .save() command and the .create() command but not when using .insertMany()
-// tourSchema.pre('save', function(next) {
-//   this.slug = slugify(this.name, { lower: true });
-//   next();
-// });
+tourSchema.pre("save", function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
 
 /*====================================================*/
 /* EXCLUDE SECRET TOURS AND SET START DATE - PRE FIND */
 /*====================================================*/
 
 // Pre-query hook for any query that starts with word "find", e.g.: find(), findOne(), findMany() etc...
-// tourSchema.pre(/^find/, function(next) {
-//   this.find({ secretTour: { $ne: true } });
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
 
-//   this.start = Date.now();
-//   next();
-// });
+  this.start = Date.now();
+  next();
+});
 
 /*=====================================*/
 /* POPULATE GUIDES IN TOURS - PRE FIND */
 /*=====================================*/
 
-// tourSchema.pre(/^find/, function(next) {
-//   this.populate({
-//     path: 'guides',
-//     select: '-__v -passwordChangedAt'
-//   });
-//   next();
-// });
+tourSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: "guides",
+    select: "-__v -passwordChangedAt"
+  });
+  next();
+});
 
 /*======================*/
 /* EXCLUDE SECRET TOURS */
@@ -153,158 +153,3 @@ tourSchema.index({ startLocation: "2dsphere" });
 const Tour = mongoose.model("Tour", tourSchema);
 
 module.exports = Tour;
-// const mongoose = require('mongoose');
-// const slugify = require('slugify');
-// const tourShape = require('./shapes/tourShape');
-// const User = require('./userModel');
-// const { fromNow } = require('../utils');
-
-// const tourSchema = new mongoose.Schema(tourShape, {
-//   timestamps: true,
-//   toJSON: { virtuals: true },
-//   toObject: { virtuals: true }
-// });
-
-// /*=========*/
-// /* INDEXES */
-// /*=========*/
-
-// // Create an index for price. When declaring a field as an index, it will be faster
-// // and more efficient for DB to search for docs based on that index
-// // Setting index to 1 means ascending order, and -1 is in descending order
-// tourSchema.index({ price: 1, ratingsAverage: -1 });
-// tourSchema.index({ slug: 1 });
-// tourSchema.index({ startLocation: '2dsphere' });
-
-// /*================================*/
-// /* SET CREATED & UPDATED FROM NOW */
-// /*================================*/
-
-// tourSchema.virtual('updatedFromNow').get(function() {
-//   if (!this.updatedAt) return;
-//   return fromNow(this.updatedAt);
-// });
-// tourSchema.virtual('createdFromNow').get(function() {
-//   if (!this.createdAt) return;
-//   return fromNow(this.createdAt);
-// });
-
-// /*============================================================*/
-// /* CREATE REVIEWS PROP ON TOUR THAT ADDS REFERENCE TO REVIEWS */
-// /*============================================================*/
-
-// // This is virtual populate for reviews. It is so that we can create a
-// // two way reference where each review refers to a tour and each tour
-// // refers to review. Instead of manually setting this relationship, we can create
-// // mongoose virtual field that does the same thing.
-// tourSchema.virtual('reviews', {
-//   ref: 'Review',
-//   foreignField: 'tour',
-//   localField: '_id'
-// });
-
-// /*=====================*/
-// /* SET SLUG - PRE SAVE */
-// /*=====================*/
-
-// // DOCUMENT MIDDLEWARE: runs before the .save() command and the .create() command but not when using .insertMany()
-// tourSchema.pre('save', function(next) {
-//   this.slug = slugify(this.name, { lower: true });
-//   next();
-// });
-
-// /*====================================================*/
-// /* EXCLUDE SECRET TOURS AND SET START DATE - PRE FIND */
-// /*====================================================*/
-
-// // Pre-query hook for any query that starts with word "find", e.g.: find(), findOne(), findMany() etc...
-// tourSchema.pre(/^find/, function(next) {
-//   this.find({ secretTour: { $ne: true } });
-
-//   this.start = Date.now();
-//   next();
-// });
-
-// /*=====================================*/
-// /* POPULATE GUIDES IN TOURS - PRE FIND */
-// /*=====================================*/
-
-// tourSchema.pre(/^find/, function(next) {
-//   this.populate({
-//     path: 'guides',
-//     select: '-__v -passwordChangedAt'
-//   });
-//   next();
-// });
-
-// /*======================*/
-// /* EXCLUDE SECRET TOURS */
-// /*======================*/
-
-// // /* Commented out for now because this one conflicts with .getDistances
-// // controller. There, when using $geoNear aggregate, it always needs to
-// // be the first ggregate in this.pipeline() array */
-// // // Exclude all secret tours when hitting 'tours/tour-stats' endpoint
-// // // AGGREGATION MIDDLEWARE
-// // tourSchema.pre('aggregate', function(next) {
-// //   console.log('this.pipeline() = ', this.pipeline());
-
-// //   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-// //   next();
-// // });
-
-// /*=====================*/
-// /* UPPERCASE TOUR NAME */
-// /*=====================*/
-
-// // // THE VIRTUAL BELOW WILL TURN NAME TO UPPERCASE
-// // tourSchema.virtual('nameUpper').get(function() {
-// //   if (this.name) return this.name.toUpperCase();
-// //   return '';
-// // });
-
-// /*==========================*/
-// /* SHORTEN DESC TO 50 CHARS */
-// /*==========================*/
-
-// // // RETURNS A SHORTER DESCRIPTION
-// // tourSchema.virtual('descShort').get(function() {
-// //   if (this.description) {
-// //     const newDesc = this.description
-// //       .split('')
-// //       .slice(0, 50)
-// //       .join('');
-// //     return `${newDesc}...`;
-// //   }
-// //   return '';
-// // });
-
-// /*===============================*/
-// /* EXAMPLE PRE & POST SAVE HOOKS */
-// /*===============================*/
-
-// // // EXAMPLE PRE SAVE HOOK - UNCOMMENT AND DO STUFF WITH IT IF NEEDED
-// // tourSchema.pre('save', function(next) {
-// //   console.log('Will save document...');
-// //   next();
-// // });
-// // // EXAMPLE AFTER SAVE HOOK - UNCOMMENT AND DO STUFF WITH IT IF NEEDED
-// // tourSchema.post('save', function(doc, next) {
-// //   console.log('doc = ', doc);
-// //   next();
-// // });
-
-// /*=====================================*/
-// /* POPULATE GUIDES IN TOURS - PRE SAVE */
-// /*=====================================*/
-
-// // // Find each user by id in guides array and reassign the guides array with array of actual User objects
-// // tourSchema.pre('save', async function(next) {
-// //   const guidesPromises = this.guides.map(async id => await User.findById(id));
-// //   this.guides = await Promise.all(guidesPromises);
-// //   next();
-// // });
-
-// const Tour = mongoose.model('Tour', tourSchema);
-
-// module.exports = Tour;
